a62ac2a0dcfcab91fb47555297f247b38a5a4ba0
Tanmay Bhuskute <tanmaybhuskute8@gmail.com>
2026-03-31 18:55:31 -0400

    remove RLDroid and uiautomator folders

--- 1_RLDroid/RLDroid-SourceCode/SeedUTGConstruction/src/main/java/com/fdu/se/sootanalyze/model/TransitionType.java
+++
@@ -1,7 +0,0 @@
-package com.fdu.se.sootanalyze.model;
-
-public class TransitionType {
-    public static String ACT_TRAN = "from_activity";
-    public static String MENU_TRAN = "from_menu";
-    public static String DIALOG_TRAN = "from_dialog";
-}
--- 1_RLDroid/RLDroid-SourceCode/SeedUTGConstruction/src/main/java/com/fdu/se/sootanalyze/model/WindowType.java
+++
@@ -1,9 +0,0 @@
-package com.fdu.se.sootanalyze.model;
-
-public class WindowType {
-    public static String ACT = "ACT";
-    public static String OPTMENU = "OPTIONSMENU";
-    public static String CTXMENU = "CONTEXTMENU";
-    public static String DIALOG = "DIALOG";
-    public static String FRAGMENT = "FRAGMENT";
-}
